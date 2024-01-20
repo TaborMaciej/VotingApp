087ab8dac3410fb6e6c3b9944d50782f721c2c2e
Maciej Tabor <b.maciejtabor@gmail.com>
2024-01-20 14:47:21 +0100

    added route for database sychronization

--- VotingWebApp/Models/KandydatDto.cs
+++ VotingWebApp/Models/KandydatDto.cs
@@ -7,9 +7,11 @@
         public string Nazwisko { get; set; }
         public string Zdjecie { get; set; }
         public string Opis { get; set; }
-        public int? NrListy { get; set; }
-        public string NazwaOkregu { get; set; }
         public Boolean czySenat { get; set; }
+        public int? NrListy { get; set; }
+        public Guid? IDKomitetu { get; set; }
+        public Guid? IDokreg { get; set; }
+        public string NazwaOkregu { get; set; }
     }
 
 }
